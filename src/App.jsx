d3c6519848f4766a720864d03e1efdd30d7500b1
vnrnesia <102244488+vnrnesia@users.noreply.github.com>
2025-02-07 21:31:44 +0300

    Learned how to change CSS styles with React.

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,7 @@ function App() {
           <code>{EXAMPLES[selectedTopic].code}</code>
         </pre>
       </div>
-      );
-
+    );
   }
 
   return (
@@ -43,10 +42,30 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleSelect("state")}
+            >
+              State
+            </TabButton>
           </menu>
 
           {tabContent}
